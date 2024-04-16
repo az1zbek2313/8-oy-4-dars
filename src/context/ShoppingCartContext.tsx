15af25a54c1e2ccb1f56dd50f54f53bfded1ded0
Azizbek:04 <150643944+az1zbek2004@users.noreply.github.com>
2024-04-16 10:02:10 +0500

    biroz o'zgartirishlar kiritildi

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -29,7 +29,7 @@ export function useShoppingCart() {
   return useContext(ShoppingCartContext);
 }
 
-export function shoppingCartProvider({ children }: ShoppingCartProviderProps) {
+export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
   const [isOpen, setIsOpen] = useState(false);
   const [cartItems, setCartItems] = useLocalStorage<CartItem[]>(
     "shopping-cart",
@@ -49,40 +49,38 @@ export function shoppingCartProvider({ children }: ShoppingCartProviderProps) {
 
   function increaseCartQuantity(id: number) {
     setCartItems((currItems) => {
-      if (currItems.find((item) => item.id === id) == null) {
-        return [...currItems, { id, quantity: 1 }];
+      const itemIndex = currItems.findIndex((item) => item.id === id);
+      if (itemIndex === -1) {
+        return [...currItems, { id, name: "", quantity: 1 }];
       } else {
-        return currItems.map((item) => {
-          if (item.id === id) {
-            return { ...item, quantity: item.quantity + 1 };
-          } else {
-            return item;
-          }
-        });
+        const updatedItems = [...currItems];
+        updatedItems[itemIndex] = {
+          ...updatedItems[itemIndex],
+          quantity: updatedItems[itemIndex].quantity + 1
+        };
+        return updatedItems;
       }
     });
   }
 
   function decreaseCartQuantity(id: number) {
     setCartItems((currItems) => {
-      if (currItems.find((item) => item.id === id)?.quantity === 1) {
+      const itemIndex = currItems.findIndex((item) => item.id === id);
+      if (itemIndex !== -1 && currItems[itemIndex].quantity === 1) {
         return currItems.filter((item) => item.id !== id);
       } else {
-        return currItems.map((item) => {
-          if (item.id === id) {
-            return { ...item, quantity: item.quantity - 1 };
-          } else {
-            return item;
-          }
-        });
+        const updatedItems = [...currItems];
+        updatedItems[itemIndex] = {
+          ...updatedItems[itemIndex],
+          quantity: updatedItems[itemIndex].quantity - 1
+        };
+        return updatedItems;
       }
     });
   }
 
   function removeFromCart(id: number) {
-    setCartItems((currItems) => {
-      return currItems.filter((item) => item.id !== id);
-    });
+    setCartItems((currItems) => currItems.filter((item) => item.id !== id));
   }
   return (
     <ShoppingCartContext.Provider
