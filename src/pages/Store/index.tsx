976630beaf44a1fa455457acdd9daf7c6bd28934
Azizbek:04 <150643944+az1zbek2004@users.noreply.github.com>
2024-04-16 10:12:41 +0500

    kodlar yozildi

--- src/pages/Store/index.tsx
+++ src/pages/Store/index.tsx
@@ -7,7 +7,7 @@ function Store() {
       <h1>Store</h1>
       <Row md={2} xs={1} lg={3} className='g-3'>
         {
-          storeItems && storeItems.map((item, index) => (
+          storeItems && storeItems.map(item => (
             <Col key={item.id}>
               <StoreItem {...item}/>  
             </Col>
